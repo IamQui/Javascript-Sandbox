4b45f580f94e51943b05bb887193eb55a52356d9
IamQui <iamquicode@gmail.com>
2024-11-17 12:14:16 -0600

    random-js

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ function topDouble(value, top) {
 }
 
 console.log(topDouble(1, 10));
+
+function maxSum(num) {
+    let total = 0;
+    for(i = 1; i <= num; i++){
+        total += i;
+    }
+    return total;   
+}
+
+console.log(maxSum(5));
+
+function endsWithX(string) {
+    if (string[string.length -1].toLowerCase() === 'x'){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//console.log(endsWithX("americanhistoryx"));
+//console.log(endsWithX("poo"));
+//console.log(endsWithX("sdlkneijvX"));
+
+function isAllX(string) {
+    for(let i = 0; i < string.length; i++) {
+        if (string[i].toLowerCase() !== "x") {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isAllX("XxxXxXXxX"));
+console.log(isAllX("XxxX0XXxX"));
+console.log(isAllX("XpooX"));
