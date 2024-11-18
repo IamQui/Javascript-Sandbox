fac23cd125705abde2edb9e17646d0bf8f44d470
IamQui <iamquicode@gmail.com>
2024-11-18 10:35:33 -0600

    random js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function topDouble(value, top) {
     while(value < top) {
         value *= 2;
@@ -16,7 +17,7 @@ function maxSum(num) {
 }
 
 console.log(maxSum(5));
-
+*/
 function endsWithX(string) {
     if (string[string.length -1].toLowerCase() === 'x'){
         return true;
@@ -38,6 +39,55 @@ function isAllX(string) {
     return true;
 }
 
-console.log(isAllX("XxxXxXXxX"));
-console.log(isAllX("XxxX0XXxX"));
-console.log(isAllX("XpooX"));
+//console.log(isAllX("XxxXxXXxX"));
+//console.log(isAllX("XxxX0XXxX"));
+//console.log(isAllX("XpooX"));
+/*
+function hasOne(array) {
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 1){
+            return true;
+        }
+         
+    }
+    return false;
+}
+
+console.log(hasOne([1,2,3]));
+console.log(hasOne([1,1,1]));
+console.log(hasOne([4,5,6]));
+*/
+
+function unique(array) {
+    let uniqueArray = [];
+    for(let i = 0; i < array.length; i++){
+        const element = array[i];
+        if(uniqueArray.indexOf(element) === -1) {
+            uniqueArray.push(element);
+        }
+    }
+    return uniqueArray;
+}
+
+//console.log(unique([1,2,3,4]));
+//console.log(unique([1,2,2,3,4,3]));
+//console.log(unique([1,1,1,1,1,1]));
+
+
+function addOne(array) {
+    for(let i = 0; i < array.length; i++){
+        array[i]++;
+    }
+}
+
+//console.log(addOne([1,2,4]));
+
+function removeOccurrences(array, num) {
+    for(let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === num) {
+            array.splice(i, 1);
+        }
+    }
+}
+
+console.log(removeOccurrences([1,2,3,1], 1));
